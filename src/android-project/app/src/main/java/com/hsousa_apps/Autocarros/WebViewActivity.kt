e48b93fd55df460cd2b37cd3ba48548b8c823e28
hc-sousa <henrique@sousadev.com>
2024-10-06 18:44:38 +0000

    Close LinearLayout button

--- src/android-project/app/src/main/java/com/hsousa_apps/Autocarros/WebViewActivity.kt
+++ src/android-project/app/src/main/java/com/hsousa_apps/Autocarros/WebViewActivity.kt
@@ -3,17 +3,24 @@ package com.hsousa_apps.Autocarros
 import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class WebViewActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
     private lateinit var btnBackToApp: Button
+    private lateinit var btnClose: Button
+    private lateinit var linearLayout: View // Reference to the LinearLayout
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     @SuppressLint("SetJavaScriptEnabled") // Suppress warning for enabling JavaScript
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +28,30 @@ class WebViewActivity : AppCompatActivity() {
 
         webView = findViewById(R.id.webView)
         btnBackToApp = findViewById(R.id.btnBackToApp)
+        btnClose = findViewById(R.id.btnClose)
+        linearLayout = findViewById(R.id.buttonLayout) // Assuming you have an ID for the LinearLayout
 
         webView.webViewClient = WebViewClient() // Ensures links are opened within the WebView
         webView.settings.javaScriptEnabled = true // Enable JavaScript if needed
+        CookieManager.getInstance().setAcceptCookie(true) // Allow the use of cookies
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
 
         // Load the desired URL
-        webView.loadUrl("https://saomiguelbus.com")
+        webView.loadUrl("https://saomiguelbus.com?device=android-app")
 
-        // Set up the button click listener
+        // Set up the button click listener for Back to App
         btnBackToApp.setOnClickListener {
             navigateToMainActivity()
         }
+
+        // Set up the button click listener for Close
+        btnClose.setOnClickListener {
+            closeLinearLayout()
+        }
+    }
+
+    private fun closeLinearLayout() {
+        linearLayout.visibility = View.GONE // Hide the LinearLayout
     }
 
     private fun navigateToMainActivity() {
@@ -50,6 +70,6 @@ class WebViewActivity : AppCompatActivity() {
             webView.goBack()
         } else {
             super.onBackPressed()
-        }
+        } 
     }
 }
